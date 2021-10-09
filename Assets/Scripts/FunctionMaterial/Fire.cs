a9404bba2d79b4049dea54fd8f75cfae8f26a542
PANJIAN <1057237562@qq.com>
2021-10-09 19:55:44 +0800

    upd

---
+++ Assets/Scripts/FunctionMaterial/Fire.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject shell;
+    public float force = 100f;
+    public Vector3 firespot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            GameObject n_s = Instantiate(shell,transform.position + firespot,transform.rotation);
+            n_s.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+        }
+    }
+}
--- Assets/Scripts/FunctionMaterial/Rotate.cs
+++ Assets/Scripts/FunctionMaterial/Rotate.cs
@@ -10,7 +10,7 @@ public class Rotate : MonoBehaviour
     {
 
     }
-
+    
     // Update is called once per frame
     void Update()
     {
