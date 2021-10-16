01f037354b2a9d26c99dd2ca4c64a371a606d3d1
PANJIAN <1057237562@qq.com>
2021-10-16 16:43:11 +0800

    upd

--- Assets/Scripts/FunctionMaterial/Fire.cs
+++ Assets/Scripts/FunctionMaterial/Fire.cs
@@ -10,7 +10,9 @@ public class Fire : MonoBehaviour
 
     public void FireFunc()
     {
-        GameObject n_s = Instantiate(shell, transform.position + firespot, transform.rotation);
-        n_s.GetComponent<Rigidbody>().AddForce(-transform.forward * force);
+        GameObject n_s = Instantiate(shell, transform.TransformPoint(firespot), transform.rotation);
+        n_s.transform.localScale = transform.localScale;
+        n_s.GetComponent<Rigidbody>().mass *= transform.localScale.x * transform.localScale.y * transform.localScale.z;
+        n_s.GetComponent<Rigidbody>().AddForce(-transform.forward * force * transform.localScale.x*transform.localScale.y*transform.localScale.z);
     }
 }
