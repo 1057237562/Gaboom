cd3551290d21f8fbf49c1f9d9fb13c1258a6f775
PANJIAN <1057237562@qq.com>
2021-10-18 17:33:50 +0800

    upd

--- Assets/Scripts/FunctionMaterial/Laser.cs
+++ Assets/Scripts/FunctionMaterial/Laser.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    public Vector3 firespot;
-    public Vector3 direction;
+    public Vector3 firespot = new Vector3(0,0.5f,2.34f);
+    public Vector3 direction = new Vector3(0,0,1);
+    public PhysicMaterial glass;
     // Update is called once per frame
     void FireIteration()
     {
-        Ray ray = new Ray(firespot,direction);
+        Ray ray = new Ray(transform.InverseTransformPoint(firespot),transform.InverseTransformDirection(direction));
         RaycastHit raycastHit;
         Physics.Raycast(ray, out raycastHit);
+        if(raycastHit.collider.material == glass)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 }
