909a6e9711971dff78ce0bfe4512240831abbdff
PANJIAN <1057237562@qq.com>
2022-01-24 16:32:10 +0800

    create Map selector

--- Assets/Scripts/GameEditor/SceneMaterial.cs
+++ Assets/Scripts/GameEditor/SceneMaterial.cs
@@ -1,6 +1,12 @@
+using Gaboom.IO;
+using Gaboom.Util;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Xml;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Gaboom.Scene
 {
@@ -8,6 +14,9 @@ namespace Gaboom.Scene
     public class SceneMaterial : MonoBehaviour
     {
         public static SceneMaterial Instance { get; private set; }
+        public static string filepath;
+
+        public Terrain terrain;
 
         public List<GameObject> prefabs = new List<GameObject>();
 
@@ -15,6 +24,22 @@ namespace Gaboom.Scene
         void Start()
         {
             Instance = this;
+            if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("GameScene") && filepath != null)
+            {
+                string dataPath = Application.dataPath + "/Workspace";
+                if (Directory.Exists(dataPath))
+                {
+                    Directory.Delete(dataPath, true);
+                }
+                Directory.CreateDirectory(dataPath);
+                LZMAHelper.DeCompress(filepath, filepath + ".upk", null);
+                UPKExtra.ExtraUPK(filepath + ".upk", dataPath, null);
+                File.Delete(filepath + ".upk");
+                terrain.terrainData.SetHeights(0, 0, FileSystem.DeserializeFromFile<float[,]>(dataPath + "/Terrain.tr"));
+                XmlDocument doc = new XmlDocument();
+                doc.Load(Application.dataPath + "/Workspace/" + Path.GetFileName(filepath));
+                SLMechanic.DeserializeToScene(doc.GetElementsByTagName("Objects")[0]);
+            }
         }
     }
 }
